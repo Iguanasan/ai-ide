10f67f707e7ebb8b2d75f40c56e6a0cb90ed821f
Glenn Euloth <glenn@euloth.com>
2025-08-13 14:36:28 -0300

    Fix ESM/CJS mismatch for Vite and Electron, add .gitignore

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // For Electron local load
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
